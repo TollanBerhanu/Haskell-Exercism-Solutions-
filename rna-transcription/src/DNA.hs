77b10d9ba0a578dd88895b87ed579228563c22b2
Tollan Berhanu <tollanberhanu@gmail.com>
2023-02-24 12:25:32 +0300

    Nucleotide count

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -13,3 +13,24 @@ convertNucleotide x
     | x == 'T' = 'A'
     | x == 'A' = 'U'
     | otherwise = x
+
+
+{- toRNA :: String -> Either Char String
+toRNA = traverse fromDNA
+  where
+    fromDNA :: Char -> Either Char Char
+    fromDNA 'G' = pure 'C'
+    fromDNA 'C' = pure 'G'
+    fromDNA 'T' = pure 'A'
+    fromDNA 'A' = pure 'U'
+    fromDNA c = Left c -}
+
+{- toRNA :: String -> Either Char String
+toRNA = traverse convert
+convert :: Char -> Either Char Char
+convert c = case c of
+        'G' -> Right 'C'
+        'C' -> Right 'G'
+        'T' -> Right 'A'
+        'A' -> Right 'U'
+        otherwise -> Left c -}
