75fa3bf563edd0a1c796ba5461d6b805599fd93c
Tollan Berhanu <tollanberhanu@gmail.com>
2023-02-27 12:39:53 +0300

    Anagram

--- strain/src/Strain.hs
+++ strain/src/Strain.hs
@@ -1,7 +1,7 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = error "You need to implement this function."
+discard p xs = foldl1 (\acc x -> if p x then acc ++ x else acc) xs
 
 keep :: (a -> Bool) -> [a] -> [a]
 keep p xs = error "You need to implement this function."
